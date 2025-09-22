6c3d288131c887e9d9d3f5dd6d3ff59764a70250
marco76tv <marco.sottana@gmail.com>
2025-09-22 18:31:17 +0200

    .

--- app/Providers/GdprServiceProvider.php
+++ app/Providers/GdprServiceProvider.php
@@ -36,5 +36,10 @@ class GdprServiceProvider extends XotBaseServiceProvider
             $router->pushMiddlewareToGroup('web', CookieConsentMiddleware::class);
         }
     }
+<<<<<<< HEAD
 >>>>>>> 111fa0e (.)
+=======
+
+    
+>>>>>>> 54ea05f (.)
 }
