a2cf8adcf281055dd646e7d11a36848e8dc46a36
marco76tv <marco.sottana@gmail.com>
2025-10-14 14:43:46 +0200

    .

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -23,8 +23,8 @@ abstract class TestCase extends BaseTestCase
     {
         parent::setUp();
 
-        // Load Gdpr module specific configurations
-        $this->loadLaravelMigrations();
+        // Run migrations for Gdpr module
+        $this->artisan('module:migrate', ['module' => 'Gdpr']);
 
         // Seed any required data for Gdpr tests
         $this->artisan('module:seed', ['module' => 'Gdpr']);
