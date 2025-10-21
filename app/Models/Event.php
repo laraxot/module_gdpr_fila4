5afb430ce58d310fbaa26c899d24ec489f4f2ab0
marco76tv <marco.sottana@gmail.com>
2025-10-21 18:30:01 +0200

    .

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -80,12 +80,12 @@ class Event extends BaseModel
         return $this->belongsTo(Consent::class);
     }
 
-    public function setPayloadAttribute(null|string $value): void
+    public function setPayloadAttribute(?string $value): void
     {
         $this->attributes['payload'] = Crypt::encrypt(json_encode($value, JSON_THROW_ON_ERROR));
     }
 
-    public function setIpAttribute(null|string $value): void
+    public function setIpAttribute(?string $value): void
     {
         $this->attributes['ip'] = Crypt::encrypt($value);
     }
