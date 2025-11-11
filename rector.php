358c33b7460f22bdd009c880af397b94f0656a40
marco76tv <marco.sottana@gmail.com>
2025-11-11 10:43:09 +0100

    .

--- rector.php
+++ rector.php
@@ -10,21 +10,6 @@ use RectorLaravel\Rector\MethodCall\RedirectRouteToToRouteHelperRector;
 use RectorLaravel\Set\LaravelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-<<<<<<< HEAD
-    $rectorConfig->paths(
-        [
-            __DIR__,
-        ]
-    );
-
-    $rectorConfig->skip(
-        [
-            './vendor/',
-            '*/docs',
-            '*/vendor',
-        ]
-    );
-=======
     $rectorConfig->paths([
         __DIR__,
     ]);
@@ -34,31 +19,12 @@ return static function (RectorConfig $rectorConfig): void {
         '*/docs',
         '*/vendor',
     ]);
->>>>>>> a074f99 (.)
 
     // register a single rule
     // $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
     // $rectorConfig->rule(RedirectRouteToToRouteHelperRector::class);
 
     // define sets of rules
-<<<<<<< HEAD
-    $rectorConfig->sets(
-        [
-            PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
-            // SetList::DEAD_CODE,
-            // SetList::CODE_QUALITY,
-            LevelSetList::UP_TO_PHP_81,
-            LaravelSetList::LARAVEL_100,
-
-            // SetList::NAMING, //problemi con injuction
-            SetList::TYPE_DECLARATION,
-            // SetList::CODING_STYLE,
-            // SetList::PRIVATIZATION,//problemi con final
-            // SetList::EARLY_RETURN,
-            // SetList::INSTANCEOF,
-        ]
-    );
-=======
     $rectorConfig->sets([
         PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
         // SetList::DEAD_CODE,
@@ -72,7 +38,6 @@ return static function (RectorConfig $rectorConfig): void {
         // SetList::EARLY_RETURN,
         // SetList::INSTANCEOF,
     ]);
->>>>>>> a074f99 (.)
 
     $rectorConfig->importNames();
 };
