9df43a44f524d5c211027c8025404f494f3a9f43
marco76tv <marco.sottana@gmail.com>
2025-11-12 09:53:17 +0100

    .

--- app/Models/BaseMorphPivot.php
+++ app/Models/BaseMorphPivot.php
@@ -56,8 +56,12 @@ abstract class BaseMorphPivot extends \Modules\Xot\Models\XotBaseMorphPivot
     /** @var list<string> */
     protected $fillable = [
         'id',
+<<<<<<< HEAD
         'post_id',
         'post_type',
+=======
+        'post_id', 'post_type',
+>>>>>>> 0c1819a (.)
         'related_type',
         'user_id',
         'note',
@@ -72,6 +76,10 @@ abstract class BaseMorphPivot extends \Modules\Xot\Models\XotBaseMorphPivot
             'created_at' => 'datetime',
             'updated_at' => 'datetime',
             'deleted_at' => 'datetime',
+<<<<<<< HEAD
+=======
+
+>>>>>>> 0c1819a (.)
             'updated_by' => 'string',
             'created_by' => 'string',
             'deleted_by' => 'string',
