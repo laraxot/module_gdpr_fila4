def36c24a16bf827b8338d219abc3423eb92d3ec
marco76tv <marco.sottana@gmail.com>
2025-11-12 09:55:02 +0100

    .

--- rector.php
+++ rector.php
@@ -10,7 +10,6 @@ use RectorLaravel\Rector\MethodCall\RedirectRouteToToRouteHelperRector;
 use RectorLaravel\Set\LaravelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-<<<<<<< HEAD
     $rectorConfig->paths([
         __DIR__,
     ]);
@@ -20,28 +19,12 @@ return static function (RectorConfig $rectorConfig): void {
         '*/docs',
         '*/vendor',
     ]);
-=======
-    $rectorConfig->paths(
-        [
-            __DIR__,
-        ]
-    );
-
-    $rectorConfig->skip(
-        [
-            './vendor/',
-            '*/docs',
-            '*/vendor',
-        ]
-    );
->>>>>>> 7f200e9 (.)
 
     // register a single rule
     // $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
     // $rectorConfig->rule(RedirectRouteToToRouteHelperRector::class);
 
     // define sets of rules
-<<<<<<< HEAD
     $rectorConfig->sets([
         PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
         // SetList::DEAD_CODE,
@@ -55,24 +38,6 @@ return static function (RectorConfig $rectorConfig): void {
         // SetList::EARLY_RETURN,
         // SetList::INSTANCEOF,
     ]);
-=======
-    $rectorConfig->sets(
-        [
-            PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
-            // SetList::DEAD_CODE,
-            // SetList::CODE_QUALITY,
-            LevelSetList::UP_TO_PHP_81,
-            LaravelSetList::LARAVEL_100,
-
-            // SetList::NAMING, //problemi con injuction
-            SetList::TYPE_DECLARATION,
-            // SetList::CODING_STYLE,
-            // SetList::PRIVATIZATION,//problemi con final
-            // SetList::EARLY_RETURN,
-            // SetList::INSTANCEOF,
-        ]
-    );
->>>>>>> 7f200e9 (.)
 
     $rectorConfig->importNames();
 };
