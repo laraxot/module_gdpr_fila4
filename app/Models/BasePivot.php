fca4dc9740e67f43a798af21e1969a66c64ce656
marco76tv <marco.sottana@gmail.com>
2025-11-12 09:52:57 +0100

    .

--- app/Models/BasePivot.php
+++ app/Models/BasePivot.php
@@ -52,6 +52,10 @@ abstract class BasePivot extends Pivot
             'created_at' => 'datetime',
             'updated_at' => 'datetime',
             'deleted_at' => 'datetime',
+<<<<<<< HEAD
+=======
+
+>>>>>>> d6fdc5d (.)
             'updated_by' => 'string',
             'created_by' => 'string',
             'deleted_by' => 'string',
