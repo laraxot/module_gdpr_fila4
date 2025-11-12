2489561ac7a75a4c64f66a428e141da2d1afe349
marco76tv <marco.sottana@gmail.com>
2025-11-12 14:54:11 +0100

    .

--- app/Models/BaseMorphPivot.php
+++ app/Models/BaseMorphPivot.php
@@ -58,17 +58,8 @@ abstract class BaseMorphPivot extends MorphPivot
     /** @var list<string> */
     protected $fillable = [
         'id',
-<<<<<<< HEAD
-<<<<<<< HEAD
         'post_id',
         'post_type',
-=======
-        'post_id', 'post_type',
->>>>>>> 0c1819a (.)
-=======
-        'post_id',
-        'post_type',
->>>>>>> ceb9f4f (.)
         'related_type',
         'user_id',
         'note',
@@ -85,13 +76,6 @@ abstract class BaseMorphPivot extends MorphPivot
             'created_at' => 'datetime',
             'updated_at' => 'datetime',
             'deleted_at' => 'datetime',
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 0c1819a (.)
-=======
->>>>>>> ceb9f4f (.)
             'updated_by' => 'string',
             'created_by' => 'string',
             'deleted_by' => 'string',
