33699ecca4c86df1a368475815bbdccd994b0070
marco76tv <marco.sottana@gmail.com>
2025-11-19 15:31:12 +0100

    .

--- app/Models/BaseMorphPivot.php
+++ app/Models/BaseMorphPivot.php
@@ -55,8 +55,12 @@ abstract class BaseMorphPivot extends MorphPivot
     /** @var list<string> */
     protected $fillable = [
         'id',
+<<<<<<< HEAD
         'post_id',
         'post_type',
+=======
+        'post_id', 'post_type',
+>>>>>>> 7f200e9 (.)
         'related_type',
         'user_id',
         'note',
@@ -71,6 +75,10 @@ abstract class BaseMorphPivot extends MorphPivot
             'created_at' => 'datetime',
             'updated_at' => 'datetime',
             'deleted_at' => 'datetime',
+<<<<<<< HEAD
+=======
+
+>>>>>>> 7f200e9 (.)
             'updated_by' => 'string',
             'created_by' => 'string',
             'deleted_by' => 'string',
--- database/factories/ConsentFactory.php
+++ database/factories/ConsentFactory.php
@@ -25,6 +25,11 @@ class ConsentFactory extends Factory
      */
     public function definition(): array
     {
+<<<<<<< HEAD
         return [];
+=======
+        return [
+        ];
+>>>>>>> 7f200e9 (.)
     }
 }
