cd34a99671add91290b6cdb72a976f3894d3a7f8
marco76tv <marco.sottana@gmail.com>
2025-12-09 10:27:27 +0100

    .

--- app/Filament/Resources/TreatmentResource/Pages/ListTreatments.php
+++ app/Filament/Resources/TreatmentResource/Pages/ListTreatments.php
@@ -16,38 +16,12 @@ class ListTreatments extends XotBaseListRecords
     public function getTableColumns(): array
     {
         return [
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ceb9f4f (.)
             'id' => TextColumn::make('id')->numeric()->sortable(),
             'name' => TextColumn::make('name')->searchable()->sortable(),
             'description' => TextColumn::make('description')->searchable()->sortable(),
             'is_active' => IconColumn::make('is_active')->boolean(),
             'data_creazione' => TextColumn::make('data_creazione')->dateTime()->sortable(),
             'data_ultima_modifica' => TextColumn::make('data_ultima_modifica')->dateTime()->sortable(),
-<<<<<<< HEAD
-=======
-            'id' => TextColumn::make('id')
-                ->numeric()
-                ->sortable(),
-            'name' => TextColumn::make('name')
-                ->searchable()
-                ->sortable(),
-            'description' => TextColumn::make('description')
-                ->searchable()
-                ->sortable(),
-            'is_active' => IconColumn::make('is_active')
-                ->boolean(),
-            'data_creazione' => TextColumn::make('data_creazione')
-                ->dateTime()
-                ->sortable(),
-            'data_ultima_modifica' => TextColumn::make('data_ultima_modifica')
-                ->dateTime()
-                ->sortable(),
->>>>>>> 0c1819a (.)
-=======
->>>>>>> ceb9f4f (.)
         ];
     }
 }
