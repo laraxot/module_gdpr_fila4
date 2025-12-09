2827df62b58b4ba4576f7090f62c2b4daf1b4584
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:02:12 +0100

    .

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -9,11 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 use Modules\Gdpr\Models\Event;
 
 /**
-<<<<<<< HEAD
  * @extends Factory<Event>
-=======
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Modules\Gdpr\Models\Event>
->>>>>>> 5a85228 (.)
  */
 class EventFactory extends Factory
 {
@@ -29,11 +25,7 @@ class EventFactory extends Factory
      */
     public function definition(): array
     {
-<<<<<<< HEAD
-        return [];
-=======
         return [
         ];
->>>>>>> 5a85228 (.)
     }
 }
