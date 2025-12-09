337876bbe462bf1a48a3eeb78b8a1fffccc514fc
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:27:02 +0100

    .

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,13 @@
+<<<<<<< HEAD
 nds('gdpr::layouts.master')
+=======
+<?php
+
+declare(strict_types=1);
+
+?>
+@extends('gdpr::layouts.master')
+>>>>>>> a074f99 (.)
 
 @section('content')
     <h1>Hello World</h1>
