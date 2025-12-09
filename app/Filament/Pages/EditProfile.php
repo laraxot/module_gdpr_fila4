4fb0ab916b1cb30cfcb1936d6659652c289af6a8
marco76tv <marco.sottana@gmail.com>
2025-12-09 10:29:25 +0100

    .

--- app/Filament/Pages/EditProfile.php
+++ app/Filament/Pages/EditProfile.php
@@ -4,13 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Gdpr\Filament\Pages;
 
-<<<<<<< HEAD
 class EditProfile extends \Filament\Auth\Pages\EditProfile
-=======
-use Filament\Pages\Auth\EditProfile as BaseEditProfile;
-
-class EditProfile extends BaseEditProfile
->>>>>>> 5a85228 (.)
 {
     protected static bool $shouldRegisterNavigation = true;
 
