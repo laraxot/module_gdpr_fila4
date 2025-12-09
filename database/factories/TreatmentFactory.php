0f77106e0f7fb7bab0e5d44b2c9a17c208c6f6d4
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:12:31 +0100

    .

--- database/factories/TreatmentFactory.php
+++ database/factories/TreatmentFactory.php
@@ -25,6 +25,11 @@ class TreatmentFactory extends Factory
      */
     public function definition(): array
     {
+<<<<<<< HEAD
         return [];
+=======
+        return [
+        ];
+>>>>>>> d6fdc5d (.)
     }
 }
