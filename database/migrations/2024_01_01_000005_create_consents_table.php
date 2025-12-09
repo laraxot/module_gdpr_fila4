e92a3984e600d48b5fb49ee9c39672cb42e34f80
marco76tv <marco.sottana@gmail.com>
2025-12-09 23:31:17 +0100

    .

--- database/migrations/2024_01_01_000005_create_consents_table.php
+++ database/migrations/2024_01_01_000005_create_consents_table.php
@@ -13,10 +13,6 @@ return new class extends XotBaseMigration {
     {
         // -- CREATE --
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ceb9f4f (.)
         $this->tableCreate(function (Blueprint $table): void {
             $table->uuid('id')->primary();
             $table->uuid('treatment_id');
@@ -51,46 +47,5 @@ return new class extends XotBaseMigration {
                 hasSoftDeletes: true,
             );
         });
-<<<<<<< HEAD
-=======
-        $this->tableCreate(
-            function (Blueprint $table): void {
-                $table->uuid('id')->primary();
-                $table->uuid('treatment_id');
-                // $table->foreignId('treatment_id')->nullable()->index();
-                $table->string('subject_id');
-
-                // $table->unique(['subject_id', 'treatment_id']);
-
-                // $table->foreign('treatment_id')->references('id')->on('gdpr_treatment');
-            }
-        );
-
-        // -- UPDATE --
-        $this->tableUpdate(
-            function (Blueprint $table): void {
-                if (! $this->hasColumn('user_id')) {
-                    $table->morphs('user');
-                }
-                if (! $this->hasColumn('type')) {
-                    $table->string('type')->nullable();
-                }
-                
-                if (! $this->hasColumn('accepted_at')) {
-                    $table->timestamp('accepted_at')->nullable();
-                }
-                 // -- Change --
-                if ($this->hasColumn('user_id')) {
-                    $table->string('user_id')->nullable()->change();
-                }
-                $table->uuid('treatment_id')->nullable()->change();
-                $table->string('subject_id')->nullable()->change();
-                
-                $this->updateTimestamps(table: $table, hasSoftDeletes: true);
-            }
-        );
->>>>>>> 0c1819a (.)
-=======
->>>>>>> ceb9f4f (.)
     }
 };
