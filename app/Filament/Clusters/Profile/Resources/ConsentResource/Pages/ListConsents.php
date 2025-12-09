26520dd79f4e58f86b5b935fa1199116b2b5ccfd
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:23:12 +0100

    .

--- app/Filament/Clusters/Profile/Resources/ConsentResource/Pages/ListConsents.php
+++ app/Filament/Clusters/Profile/Resources/ConsentResource/Pages/ListConsents.php
@@ -19,48 +19,12 @@ class ListConsents extends XotBaseListRecords
     public function getTableColumns(): array
     {
         return [
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 5562af7 (.)
             'id' => TextColumn::make('id')->numeric()->sortable(),
             'treatment_id' => TextColumn::make('treatment.name')->sortable(),
             'subject_id' => TextColumn::make('subject.name')->sortable(),
             'is_accepted' => IconColumn::make('is_accepted')->boolean(),
             'data_creazione' => TextColumn::make('data_creazione')->dateTime()->sortable(),
             'data_ultima_modifica' => TextColumn::make('data_ultima_modifica')->dateTime()->sortable(),
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> origin/develop
-            'id' => TextColumn::make('id')
-                ->numeric()
-                ->sortable(),
-            'treatment_id' => TextColumn::make('treatment.name')
-                ->sortable(),
-            'subject_id' => TextColumn::make('subject.name')
-                ->sortable(),
-            'is_accepted' => IconColumn::make('is_accepted')
-                ->boolean(),
-            'data_creazione' => TextColumn::make('data_creazione')
-                ->dateTime()
-                ->sortable(),
-            'data_ultima_modifica' => TextColumn::make('data_ultima_modifica')
-                ->dateTime()
-                ->sortable(),
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 5562af7 (.)
         ];
     }
 }
