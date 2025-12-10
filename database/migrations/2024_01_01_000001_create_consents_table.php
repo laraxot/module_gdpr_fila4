d73bd635b6fd908cc81405969613d0b90a9dc36a
marco76tv <marco.sottana@gmail.com>
2025-12-10 09:11:17 +0100

    .

--- database/migrations/2024_01_01_000001_create_consents_table.php
+++ database/migrations/2024_01_01_000001_create_consents_table.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 use Illuminate\Database\Schema\Blueprint;
 use Modules\Xot\Database\Migrations\XotBaseMigration;
 
+<<<<<<< HEAD
 return new class() extends XotBaseMigration
 {
+=======
+return new class() extends XotBaseMigration {
+>>>>>>> a074f99 (.)
     /**
      * Run the migrations.
      */
@@ -14,6 +18,7 @@ return new class() extends XotBaseMigration
     {
         // -- CREATE --
 
+<<<<<<< HEAD
         $this->tableCreate(
             function (Blueprint $table): void {
                 $table->uuid('id')->primary();
@@ -36,5 +41,27 @@ return new class() extends XotBaseMigration
                 $this->updateTimestamps(table: $table, hasSoftDeletes: true);
             }
         );
+=======
+        $this->tableCreate(function (Blueprint $table): void {
+            $table->uuid('id')->primary();
+            $table->uuid('treatment_id');
+            // $table->foreignId('treatment_id')->nullable()->index();
+            $table->string('subject_id');
+
+            // $table->unique(['subject_id', 'treatment_id']);
+            // $table->foreign('treatment_id')->references('id')->on('gdpr_treatment');
+        });
+
+        // -- UPDATE --
+        $this->tableUpdate(function (Blueprint $table): void {
+            // if (! $this->hasColumn('email')) {
+            //    $table->string('email')->nullable();
+            // }
+            $this->updateTimestamps(
+                table: $table,
+                hasSoftDeletes: true,
+            );
+        });
+>>>>>>> a074f99 (.)
     }
 };
