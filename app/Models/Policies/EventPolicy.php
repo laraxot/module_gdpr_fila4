902f7cf36d116a0c5bf8c8ab6c3731eccbcc46d1
marco76tv <marco.sottana@gmail.com>
2025-12-10 11:01:32 +0100

    .

--- app/Models/Policies/EventPolicy.php
+++ app/Models/Policies/EventPolicy.php
@@ -20,11 +20,7 @@ class EventPolicy extends GdprBasePolicy
     /**
      * Determine whether the user can view the model.
      */
-<<<<<<< HEAD
-    public function view(UserContract $user, Event $_event): bool
-=======
     public function view(UserContract $user, Event $event): bool
->>>>>>> 5a85228 (.)
     {
         return $user->hasPermissionTo('event.view');
     }
@@ -40,11 +36,7 @@ class EventPolicy extends GdprBasePolicy
     /**
      * Determine whether the user can update the model.
      */
-<<<<<<< HEAD
-    public function update(UserContract $user, Event $_event): bool
-=======
     public function update(UserContract $user, Event $event): bool
->>>>>>> 5a85228 (.)
     {
         return $user->hasPermissionTo('event.update');
     }
@@ -52,11 +44,7 @@ class EventPolicy extends GdprBasePolicy
     /**
      * Determine whether the user can delete the model.
      */
-<<<<<<< HEAD
-    public function delete(UserContract $user, Event $_event): bool
-=======
     public function delete(UserContract $user, Event $event): bool
->>>>>>> 5a85228 (.)
     {
         return $user->hasPermissionTo('event.delete');
     }
@@ -64,11 +52,7 @@ class EventPolicy extends GdprBasePolicy
     /**
      * Determine whether the user can restore the model.
      */
-<<<<<<< HEAD
-    public function restore(UserContract $user, Event $_event): bool
-=======
     public function restore(UserContract $user, Event $event): bool
->>>>>>> 5a85228 (.)
     {
         return $user->hasPermissionTo('event.restore');
     }
@@ -80,8 +64,4 @@ class EventPolicy extends GdprBasePolicy
     {
         return $user->hasPermissionTo('event.forceDelete');
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 5a85228 (.)
