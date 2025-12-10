269426e91817117e47e54b44a9684a8fa6b810c9
marco76tv <marco.sottana@gmail.com>
2025-12-10 11:09:49 +0100

    .

--- app/Filament/Clusters/Profile.php
+++ app/Filament/Clusters/Profile.php
@@ -8,5 +8,9 @@ use Filament\Clusters\Cluster;
 
 class Profile extends Cluster
 {
+<<<<<<< HEAD
     protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-squares-2x2';
+=======
+    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+>>>>>>> 5a85228 (.)
 }
--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -8,7 +8,11 @@ use Modules\Xot\Filament\Pages\XotBaseDashboard;
 
 class Dashboard extends XotBaseDashboard
 {
+<<<<<<< HEAD
     protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-home';
+=======
+    protected static ?string $navigationIcon = 'heroicon-o-home';
+>>>>>>> 5a85228 (.)
 
     // protected static string $view = 'gdpr::filament.pages.dashboard';
 }
