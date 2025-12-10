eac6a5432a400ff1846e2d7a89b8efce4e7f8b8d
marco76tv <marco.sottana@gmail.com>
2025-12-10 10:49:36 +0100

    .

--- app/Filament/Resources/TreatmentResource/Pages/ListTreatments.php
+++ app/Filament/Resources/TreatmentResource/Pages/ListTreatments.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Gdpr\Filament\Resources\TreatmentResource\Pages;
 
+<<<<<<< HEAD
 use Filament\Tables\Columns\Column;
+=======
+>>>>>>> a074f99 (.)
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Modules\Gdpr\Filament\Resources\TreatmentResource;
@@ -16,6 +19,7 @@ class ListTreatments extends XotBaseListRecords
 
     public function getTableColumns(): array
     {
+<<<<<<< HEAD
         // Column types are inferred by Filament v4
         return [
             'id' => TextColumn::make('id')
@@ -35,6 +39,15 @@ class ListTreatments extends XotBaseListRecords
             'data_ultima_modifica' => TextColumn::make('data_ultima_modifica')
                 ->dateTime()
                 ->sortable(),
+=======
+        return [
+            'id' => TextColumn::make('id')->numeric()->sortable(),
+            'name' => TextColumn::make('name')->searchable()->sortable(),
+            'description' => TextColumn::make('description')->searchable()->sortable(),
+            'is_active' => IconColumn::make('is_active')->boolean(),
+            'data_creazione' => TextColumn::make('data_creazione')->dateTime()->sortable(),
+            'data_ultima_modifica' => TextColumn::make('data_ultima_modifica')->dateTime()->sortable(),
+>>>>>>> a074f99 (.)
         ];
     }
 }
