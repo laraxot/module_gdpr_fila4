4a95f491d3d1141f02a2403d7873c536fce95549
marco76tv <marco.sottana@gmail.com>
2025-12-10 10:43:25 +0100

    .

--- app/Filament/Resources/ConsentResource/Pages/ListConsents.php
+++ app/Filament/Resources/ConsentResource/Pages/ListConsents.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Gdpr\Filament\Resources\ConsentResource\Pages;
 
+use Filament\Tables\Columns\Column;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Modules\Gdpr\Filament\Resources\ConsentResource;
@@ -15,16 +16,7 @@ class ListConsents extends XotBaseListRecords
 
     public function getTableColumns(): array
     {
-<<<<<<< HEAD
-        return [
-            'id' => TextColumn::make('id')->numeric()->sortable(),
-            'treatment_id' => TextColumn::make('treatment.name')->sortable(),
-            'subject_id' => TextColumn::make('subject.name')->sortable(),
-            'is_accepted' => IconColumn::make('is_accepted')->boolean(),
-            'data_creazione' => TextColumn::make('data_creazione')->dateTime()->sortable(),
-            'data_ultima_modifica' => TextColumn::make('data_ultima_modifica')->dateTime()->sortable(),
-=======
-        /** @var array<string, \Filament\Tables\Columns\Column> */
+        /** @var array<string, Column> */
         return [
             'id' => TextColumn::make('id')
                 ->numeric()
@@ -41,7 +33,6 @@ class ListConsents extends XotBaseListRecords
             'data_ultima_modifica' => TextColumn::make('data_ultima_modifica')
                 ->dateTime()
                 ->sortable(),
->>>>>>> 5a85228 (.)
         ];
     }
 }
