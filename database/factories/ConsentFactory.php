5863b66b5784ef2cb3fb22e6fe9d54e94f71d11f
marco76tv <marco.sottana@gmail.com>
2025-12-10 10:47:40 +0100

    .

--- database/factories/ConsentFactory.php
+++ database/factories/ConsentFactory.php
@@ -9,7 +9,15 @@ use Illuminate\Database\Eloquent\Model;
 use Modules\Gdpr\Models\Consent;
 
 /**
+<<<<<<< HEAD
  * @extends Factory<Consent>
+=======
+<<<<<<< HEAD
+ * @extends Factory<Consent>
+=======
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Modules\Gdpr\Models\Consent>
+>>>>>>> origin/develop
+>>>>>>> 5562af7 (.)
  */
 class ConsentFactory extends Factory
 {
@@ -25,6 +33,24 @@ class ConsentFactory extends Factory
      */
     public function definition(): array
     {
+<<<<<<< HEAD
         return [];
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+        return [];
+=======
+        return [
+        ];
+>>>>>>> a12f125f4a (.)
+=======
+        return [];
+>>>>>>> b93ef594b4 (.)
+=======
+        return [
+        ];
+>>>>>>> origin/develop
+>>>>>>> 5562af7 (.)
     }
 }
--- database/migrations/2024_01_01_000001_create_treatments_table.php
+++ database/migrations/2024_01_01_000001_create_treatments_table.php
@@ -14,6 +14,14 @@ return new class() extends XotBaseMigration
     {
         // -- CREATE --
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 5562af7 (.)
         $this->tableCreate(function (Blueprint $table): void {
             $table->uuid('id')->primary();
             $table->boolean('active')->default(true);
@@ -24,6 +32,10 @@ return new class() extends XotBaseMigration
             $table->string('documentUrl')->nullable()->default(null);
             $table->tinyInteger('weight');
         });
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> 5562af7 (.)
 
         // -- UPDATE --
         $this->tableUpdate(function (Blueprint $table): void {
@@ -35,5 +47,50 @@ return new class() extends XotBaseMigration
                 hasSoftDeletes: true,
             );
         });
+<<<<<<< HEAD
+=======
+=======
+=======
+>>>>>>> origin/develop
+        $this->tableCreate(
+            function (Blueprint $table): void {
+                $table->uuid('id')->primary();
+                $table->boolean('active')->default(true);
+                $table->boolean('required')->default(true);
+                $table->string('name')->unique();
+                $table->text('description');
+                $table->string('documentVersion')->nullable()->default(null);
+                $table->string('documentUrl')->nullable()->default(null);
+                $table->tinyInteger('weight');
+            }
+        );
+
+        // -- UPDATE --
+        $this->tableUpdate(
+            function (Blueprint $table): void {
+                // if (! $this->hasColumn('email')) {
+                //    $table->string('email')->nullable();
+                // }
+                $this->updateTimestamps(table: $table, hasSoftDeletes: true);
+            }
+        );
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+
+        // -- UPDATE --
+        $this->tableUpdate(function (Blueprint $table): void {
+            // if (! $this->hasColumn('email')) {
+            //    $table->string('email')->nullable();
+            // }
+            $this->updateTimestamps(
+                table: $table,
+                hasSoftDeletes: true,
+            );
+        });
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 5562af7 (.)
     }
 };
