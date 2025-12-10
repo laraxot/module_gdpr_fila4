7e3253e7cc85afbb17f4eda4be49f3c6eb1fad68
marco76tv <marco.sottana@gmail.com>
2025-12-10 10:57:59 +0100

    .

--- app/Filament/Resources/EventResource/Pages/ListEvents.php
+++ app/Filament/Resources/EventResource/Pages/ListEvents.php
@@ -31,20 +31,8 @@ class ListEvents extends XotBaseListRecords
                 ->numeric()
                 ->sortable()
                 ->searchable(),
-<<<<<<< HEAD
-<<<<<<< HEAD
             'ip' => TextColumn::make('ip')->searchable(),
             'action' => TextColumn::make('action')->searchable(),
-=======
-            'ip' => TextColumn::make('ip')
-                ->searchable(),
-            'action' => TextColumn::make('action')
-                ->searchable(),
->>>>>>> 0c1819a (.)
-=======
-            'ip' => TextColumn::make('ip')->searchable(),
-            'action' => TextColumn::make('action')->searchable(),
->>>>>>> ceb9f4f (.)
             'created_at' => TextColumn::make('created_at')
                 ->dateTime()
                 ->sortable()
--- database/migrations/2024_01_01_000001_create_treatments_table.php
+++ database/migrations/2024_01_01_000001_create_treatments_table.php
@@ -14,10 +14,6 @@ return new class() extends XotBaseMigration
     {
         // -- CREATE --
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ceb9f4f (.)
         $this->tableCreate(function (Blueprint $table): void {
             $table->uuid('id')->primary();
             $table->boolean('active')->default(true);
@@ -39,32 +35,5 @@ return new class() extends XotBaseMigration
                 hasSoftDeletes: true,
             );
         });
-<<<<<<< HEAD
-=======
-        $this->tableCreate(
-            function (Blueprint $table): void {
-                $table->uuid('id')->primary();
-                $table->boolean('active')->default(true);
-                $table->boolean('required')->default(true);
-                $table->string('name')->unique();
-                $table->text('description');
-                $table->string('documentVersion')->nullable()->default(null);
-                $table->string('documentUrl')->nullable()->default(null);
-                $table->tinyInteger('weight');
-            }
-        );
-
-        // -- UPDATE --
-        $this->tableUpdate(
-            function (Blueprint $table): void {
-                // if (! $this->hasColumn('email')) {
-                //    $table->string('email')->nullable();
-                // }
-                $this->updateTimestamps(table: $table, hasSoftDeletes: true);
-            }
-        );
->>>>>>> 0c1819a (.)
-=======
->>>>>>> ceb9f4f (.)
     }
 };
