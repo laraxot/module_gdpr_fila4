ee08c85bc3d5925b0cbe2a4403e7d12c8e186604
marco76tv <marco.sottana@gmail.com>
2025-12-10 09:11:29 +0100

    .

--- database/migrations/2024_01_01_000001_create_consents_table.php
+++ database/migrations/2024_01_01_000001_create_consents_table.php
@@ -5,24 +5,7 @@ declare(strict_types=1);
 use Illuminate\Database\Schema\Blueprint;
 use Modules\Xot\Database\Migrations\XotBaseMigration;
 
-<<<<<<< HEAD
 return new class() extends XotBaseMigration {
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-return new class() extends XotBaseMigration {
-=======
-return new class() extends XotBaseMigration
-{
->>>>>>> a12f125f4a (.)
-=======
-return new class() extends XotBaseMigration {
->>>>>>> b93ef594b4 (.)
-=======
-return new class extends XotBaseMigration {
->>>>>>> origin/develop
->>>>>>> 5562af7 (.)
     /**
      * Run the migrations.
      */
@@ -30,23 +13,11 @@ return new class extends XotBaseMigration {
     {
         // -- CREATE --
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 5562af7 (.)
         $this->tableCreate(function (Blueprint $table): void {
             $table->uuid('id')->primary();
             $table->uuid('treatment_id');
             // $table->foreignId('treatment_id')->nullable()->index();
             $table->string('subject_id');
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 5562af7 (.)
 
             // $table->unique(['subject_id', 'treatment_id']);
             // $table->foreign('treatment_id')->references('id')->on('gdpr_treatment');
@@ -62,60 +33,5 @@ return new class extends XotBaseMigration {
                 hasSoftDeletes: true,
             );
         });
-<<<<<<< HEAD
-=======
-=======
-=======
->>>>>>> origin/develop
-        $this->tableCreate(
-            function (Blueprint $table): void {
-                $table->uuid('id')->primary();
-                $table->uuid('treatment_id');
-                // $table->foreignId('treatment_id')->nullable()->index();
-                $table->string('subject_id');
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
-
-            // $table->unique(['subject_id', 'treatment_id']);
-            // $table->foreign('treatment_id')->references('id')->on('gdpr_treatment');
-        });
-
-        // -- UPDATE --
-<<<<<<< HEAD
-=======
-
-                // $table->unique(['subject_id', 'treatment_id']);
-
-                // $table->foreign('treatment_id')->references('id')->on('gdpr_treatment');
-            }
-        );
-
-        // -- UPDATE --
->>>>>>> origin/develop
-        $this->tableUpdate(
-            function (Blueprint $table): void {
-                // if (! $this->hasColumn('email')) {
-                //    $table->string('email')->nullable();
-                // }
-                $this->updateTimestamps(table: $table, hasSoftDeletes: true);
-            }
-        );
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
-        $this->tableUpdate(function (Blueprint $table): void {
-            // if (! $this->hasColumn('email')) {
-            //    $table->string('email')->nullable();
-            // }
-            $this->updateTimestamps(
-                table: $table,
-                hasSoftDeletes: true,
-            );
-        });
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 5562af7 (.)
     }
 };
