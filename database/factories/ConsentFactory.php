9ad248848bb7f3e84e5bf1ec819f8a0959691da9
marco76tv <marco.sottana@gmail.com>
2025-12-10 11:10:17 +0100

    .

--- database/factories/ConsentFactory.php
+++ database/factories/ConsentFactory.php
@@ -25,11 +25,6 @@ class ConsentFactory extends Factory
      */
     public function definition(): array
     {
-<<<<<<< HEAD
         return [];
-=======
-        return [
-        ];
->>>>>>> d6fdc5d (.)
     }
 }
