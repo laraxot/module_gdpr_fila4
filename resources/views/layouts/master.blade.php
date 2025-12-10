9220697793ea5456366e00af63a89b9b943af007
marco76tv <marco.sottana@gmail.com>
2025-12-10 10:41:52 +0100

    .

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,11 +1,17 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> ceb9f4f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
 =======
 >>>>>>> 0c1819a (.)
+=======
+>>>>>>> ceb9f4f (.)
 <!DOCTYPE html>
 =======
 TYPE html>
