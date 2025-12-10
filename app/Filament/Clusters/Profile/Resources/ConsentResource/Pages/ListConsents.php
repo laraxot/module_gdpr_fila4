7951569e9d45704a30a62ed419276423b14a95ae
marco76tv <marco.sottana@gmail.com>
2025-12-10 10:49:01 +0100

    .

--- app/Filament/Clusters/Profile/Resources/ConsentResource/Pages/ListConsents.php
+++ app/Filament/Clusters/Profile/Resources/ConsentResource/Pages/ListConsents.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Gdpr\Filament\Clusters\Profile\Resources\ConsentResource\Pages;
 
+<<<<<<< HEAD
+=======
+use Filament\Tables\Columns\Column;
+>>>>>>> 7f200e9 (.)
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Modules\Gdpr\Filament\Clusters\Profile\Resources\ConsentResource;
@@ -15,6 +19,7 @@ class ListConsents extends XotBaseListRecords
 
     public function getTableColumns(): array
     {
+<<<<<<< HEAD
         return [
             'id' => TextColumn::make('id')->numeric()->sortable(),
             'treatment_id' => TextColumn::make('treatment.name')->sortable(),
@@ -22,6 +27,25 @@ class ListConsents extends XotBaseListRecords
             'is_accepted' => IconColumn::make('is_accepted')->boolean(),
             'data_creazione' => TextColumn::make('data_creazione')->dateTime()->sortable(),
             'data_ultima_modifica' => TextColumn::make('data_ultima_modifica')->dateTime()->sortable(),
+=======
+        // Column types are inferred by Filament v4
+        return [
+            'id' => TextColumn::make('id')
+                ->numeric()
+                ->sortable(),
+            'treatment_id' => TextColumn::make('treatment.name')
+                ->sortable(),
+            'subject_id' => TextColumn::make('subject.name')
+                ->sortable(),
+            'is_accepted' => IconColumn::make('is_accepted')
+                ->boolean(),
+            'data_creazione' => TextColumn::make('data_creazione')
+                ->dateTime()
+                ->sortable(),
+            'data_ultima_modifica' => TextColumn::make('data_ultima_modifica')
+                ->dateTime()
+                ->sortable(),
+>>>>>>> 7f200e9 (.)
         ];
     }
 }
