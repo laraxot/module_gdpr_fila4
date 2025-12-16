3e1322a5c1c768f9349fd290a36bc0214250e69e
marco76tv <marco.sottana@gmail.com>
2025-12-16 19:32:50 +0100

    .

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -25,11 +25,6 @@ class EventFactory extends Factory
      */
     public function definition(): array
     {
-<<<<<<< HEAD
-        return [
-        ];
-=======
         return [];
->>>>>>> a074f99 (.)
     }
 }
