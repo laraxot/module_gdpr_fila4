838d8d638c9f4438861fbdaaac6ca24c597a39b2
marco76tv <marco.sottana@gmail.com>
2025-12-16 19:21:41 +0100

    .

--- app/Filament/Resources/ConsentResource/Pages/ListConsents.php
+++ app/Filament/Resources/ConsentResource/Pages/ListConsents.php
@@ -16,9 +16,6 @@ class ListConsents extends XotBaseListRecords
 {
     protected static string $resource = ConsentResource::class;
 
-    /**
-     * @return array<string, mixed>
-     */
     public function getTableColumns(): array
     {
         return [
