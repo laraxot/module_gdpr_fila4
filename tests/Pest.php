e941dd62a35d2024518e3b7e2f92e54fb01bb257
marco76tv <marco.sottana@gmail.com>
2026-01-08 09:34:12 +0100

    .

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,69 +2,8 @@
 
 declare(strict_types=1);
 
-<<<<<<< HEAD
 use Modules\Gdpr\Models\GdprConsent;
 use Modules\Gdpr\Models\GdprRequest;
-use Modules\Gdpr\Tests\TestCase;
-
-/*
- * |--------------------------------------------------------------------------
- * | Test Case
- * |--------------------------------------------------------------------------
- * |
- * | The closure you provide to your test functions is always bound to a specific PHPUnit test
- * | case class. By default, that class is "PHPUnit\Framework\TestCase". Of course, you may
- * | need to change it using the "pest()" function to bind a different classes or traits.
- * |
- */
-
-pest()->extend(TestCase::class)->in('Feature', 'Unit');
-
-/*
- * |--------------------------------------------------------------------------
- * | Expectations
- * |--------------------------------------------------------------------------
- * |
- * | When you're writing tests, you often need to check that values meet certain conditions. The
- * | "expect()" function gives you access to a set of "expectations" methods that you can use
- * | to assert different things. Of course, you may extend the Expectation API at any time.
- * |
- */
-
-expect()->extend('toBeGdprConsent', fn () => $this->toBeInstanceOf(GdprConsent::class));
-
-expect()->extend('toBeGdprRequest', fn () => $this->toBeInstanceOf(GdprRequest::class));
-
-/*
- * |--------------------------------------------------------------------------
- * | Functions
- * |--------------------------------------------------------------------------
- * |
- * | While Pest is very powerful out-of-the-box, you may have some testing code specific to your
- * | project that you don't want to repeat in every file. Here you can also expose helpers as
- * | global functions to help you to reduce the number of lines of code in your test files.
- * |
- */
-
-function createGdprConsent(array $attributes = []): GdprConsent
-{
-    return GdprConsent::factory()->create($attributes);
-}
-
-function makeGdprConsent(array $attributes = []): GdprConsent
-{
-    return GdprConsent::factory()->make($attributes);
-}
-
-function createGdprRequest(array $attributes = []): GdprRequest
-{
-    return GdprRequest::factory()->create($attributes);
-}
-
-function makeGdprRequest(array $attributes = []): GdprRequest
-{
-    return GdprRequest::factory()->make($attributes);
-=======
 use Modules\Gdpr\Tests\TestCase;
 
 /*
@@ -93,11 +32,11 @@ pest()->extend(TestCase::class)
 */
 
 expect()->extend('toBeGdprConsent', function () {
-    return $this->toBeInstanceOf(\Modules\Gdpr\Models\GdprConsent::class);
+    return $this->toBeInstanceOf(GdprConsent::class);
 });
 
 expect()->extend('toBeGdprRequest', function () {
-    return $this->toBeInstanceOf(\Modules\Gdpr\Models\GdprRequest::class);
+    return $this->toBeInstanceOf(GdprRequest::class);
 });
 
 /*
@@ -111,23 +50,22 @@ expect()->extend('toBeGdprRequest', function () {
 |
 */
 
-function createGdprConsent(array $attributes = []): \Modules\Gdpr\Models\GdprConsent
+function createGdprConsent(array $attributes = []): GdprConsent
 {
-    return \Modules\Gdpr\Models\GdprConsent::factory()->create($attributes);
+    return GdprConsent::factory()->create($attributes);
 }
 
-function makeGdprConsent(array $attributes = []): \Modules\Gdpr\Models\GdprConsent
+function makeGdprConsent(array $attributes = []): GdprConsent
 {
-    return \Modules\Gdpr\Models\GdprConsent::factory()->make($attributes);
+    return GdprConsent::factory()->make($attributes);
 }
 
-function createGdprRequest(array $attributes = []): \Modules\Gdpr\Models\GdprRequest
+function createGdprRequest(array $attributes = []): GdprRequest
 {
-    return \Modules\Gdpr\Models\GdprRequest::factory()->create($attributes);
+    return GdprRequest::factory()->create($attributes);
 }
 
-function makeGdprRequest(array $attributes = []): \Modules\Gdpr\Models\GdprRequest
+function makeGdprRequest(array $attributes = []): GdprRequest
 {
-    return \Modules\Gdpr\Models\GdprRequest::factory()->make($attributes);
->>>>>>> 5a85228 (.)
+    return GdprRequest::factory()->make($attributes);
 }
