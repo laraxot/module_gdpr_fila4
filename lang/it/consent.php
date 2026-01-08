6fbc324f00455e34a150dd143c3bbebd767b895e
marco76tv <marco.sottana@gmail.com>
2026-01-08 09:22:04 +0100

    .

--- lang/it/consent.php
+++ lang/it/consent.php
@@ -1,9 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ceb9f4f (.)
 declare(strict_types=1);
 
 return [
@@ -48,58 +44,3 @@ return [
         'renew' => 'Rinnova',
     ],
 ];
-<<<<<<< HEAD
-=======
-return array (
-  'navigation' => 
-  array (
-    'name' => 'Consensi',
-    'plural' => 'Consensi',
-    'group' => 
-    array (
-      'name' => 'GDPR',
-      'description' => 'Gestione dei consensi privacy',
-    ),
-    'label' => 'Gestione Consensi',
-    'sort' => 62,
-    'icon' => 'gdpr-consent',
-  ),
-  'fields' => 
-  array (
-    'user' => 'Utente',
-    'type' => 'Tipo Consenso',
-    'status' => 'Stato',
-    'date' => 'Data',
-    'ip_address' => 'Indirizzo IP',
-    'notes' => 'Note',
-    'toggleColumns' => 
-    array (
-      'label' => 'toggleColumns',
-    ),
-    'reorderRecords' => 
-    array (
-      'label' => 'reorderRecords',
-    ),
-    'resetFilters' => 
-    array (
-      'label' => 'resetFilters',
-    ),
-  ),
-  'statuses' => 
-  array (
-    'granted' => 'Concesso',
-    'denied' => 'Negato',
-    'withdrawn' => 'Revocato',
-    'expired' => 'Scaduto',
-  ),
-  'actions' => 
-  array (
-    'grant' => 'Concedi',
-    'deny' => 'Nega',
-    'withdraw' => 'Revoca',
-    'renew' => 'Rinnova',
-  ),
-);
->>>>>>> 0c1819a (.)
-=======
->>>>>>> ceb9f4f (.)
