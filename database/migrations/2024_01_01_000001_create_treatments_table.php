be7e47cac76fa027d8bf07eadb88adf73b47080b
marco76tv <marco.sottana@gmail.com>
2026-01-08 09:19:58 +0100

    .

--- database/migrations/2024_01_01_000001_create_treatments_table.php
+++ database/migrations/2024_01_01_000001_create_treatments_table.php
@@ -14,30 +14,6 @@ return new class extends XotBaseMigration
     {
         // -- CREATE --
 
-<<<<<<< HEAD
-        $this->tableCreate(
-            function (Blueprint $table): void {
-                $table->uuid('id')->primary();
-                $table->boolean('active')->default(true);
-                $table->boolean('required')->default(true);
-                $table->string('name')->unique();
-                $table->text('description');
-                $table->string('documentVersion')->nullable()->default(null);
-                $table->string('documentUrl')->nullable()->default(null);
-                $table->tinyInteger('weight');
-            }
-        );
-
-        // -- UPDATE --
-        $this->tableUpdate(
-            function (Blueprint $table): void {
-                // if (! $this->hasColumn('email')) {
-                //    $table->string('email')->nullable();
-                // }
-                $this->updateTimestamps(table: $table, hasSoftDeletes: true);
-            }
-        );
-=======
         $this->tableCreate(function (Blueprint $table): void {
             $table->uuid('id')->primary();
             $table->boolean('active')->default(true);
@@ -59,6 +35,5 @@ return new class extends XotBaseMigration
                 hasSoftDeletes: true,
             );
         });
->>>>>>> a074f99 (.)
     }
 };
