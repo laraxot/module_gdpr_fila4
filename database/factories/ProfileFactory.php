d97a577b2e79c5b3630652fc9ffd178a0ebc2ff1
marco76tv <marco.sottana@gmail.com>
2026-01-08 09:30:32 +0100

    .

--- database/factories/ProfileFactory.php
+++ database/factories/ProfileFactory.php
@@ -23,10 +23,16 @@ class ProfileFactory extends Factory
     /**
      * Define the model's default state.
      *
+<<<<<<< HEAD
      * @return array
      */
     public function definition()
     {
+=======
+     * @return array<string, mixed>
+     */
+    public function definition(): array {
+>>>>>>> d6fdc5d (.)
         return [
             'id' => fake()->word,
             'user_id' => fake()->unique()->randomNumber(),
