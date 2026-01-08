1a681910352f96749711282cd2904a853ec6ee88
marco76tv <marco.sottana@gmail.com>
2026-01-08 09:30:36 +0100

    .

--- app/Filament/Resources/ConsentResource.php
+++ app/Filament/Resources/ConsentResource.php
@@ -6,6 +6,7 @@ namespace Modules\Gdpr\Filament\Resources;
 
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\TextColumn;
 use Modules\Gdpr\Filament\Resources\ConsentResource\Pages\CreateConsent;
 use Modules\Gdpr\Filament\Resources\ConsentResource\Pages\EditConsent;
 use Modules\Gdpr\Filament\Resources\ConsentResource\Pages\ListConsents;
@@ -22,12 +23,29 @@ class ConsentResource extends XotBaseResource
     #[Override]
     public static function getFormSchema(): array
     {
-        return array_values([
+        return [
             'treatment_id' => Select::make('treatment_id')
                 ->relationship('treatment', 'name')
                 ->required(),
             'subject_id' => TextInput::make('subject_id')->required()->maxLength(191),
-        ]);
+        ];
+    }
+
+    public function getTableColumns(): array
+    {
+        return [
+            TextColumn::make('id')->searchable(),
+            TextColumn::make('treatment.name')->searchable(),
+            TextColumn::make('subject_id')->searchable(),
+            TextColumn::make('created_at')
+                ->dateTime()
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+            TextColumn::make('updated_at')
+                ->dateTime()
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+        ];
     }
 
     #[Override]
